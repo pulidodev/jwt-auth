0d5926a263b9e73a61249b0638f576846f4643e3
Francisco Pulido <pulidodev@gmail.com>
2023-02-05 19:13:12 -0300

    first commit

---
+++ src/main/java/com/pulidodev/security/model/Role.java
@@ -0,0 +1,6 @@
+package com.pulidodev.security.model;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ src/main/java/com/pulidodev/security/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.pulidodev.security.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pulidodev.security.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
